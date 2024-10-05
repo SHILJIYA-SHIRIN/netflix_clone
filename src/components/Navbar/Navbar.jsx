b43be4beda0fb09b490d2595e01f26be0a632c8a
SHILJIYA SHIRIN <sshiljiya@gmail.com>
2024-10-05 13:03:30 +0530

    created Navbar component with Navbarr.jsx and Navbar.css

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import "./Navbar.css"
+import logo from '../../assets/logo.png'
+import searchIcon from '../../assets/search_icon.svg'
+import bellIcon from '../../assets/bell_icon.svg'
+import dropDownIcon from '../../assets/caret_icon.svg'
+import profileImage from '../../assets/profile_img.png'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+    <div className="navbar-left">
+      <img src={logo} alt="" />
+      <ul>
+        <li>Home</li>
+        <li>TV shows</li>
+        <li>Movies</li>
+        <li>New & Popular</li>
+        <li>My List</li>
+        <li>Browse By Language</li>
+      </ul>
+    </div>
+    <div className="navbar-right">
+      <img src={searchIcon} alt="" className='icons' />
+      <p>Children</p>
+      <img src={bellIcon} alt="" className='icons' />
+    <div className="navbar-profile">
+    <img src={profileImage} alt="" className='profile' />
+    <img src={dropDownIcon} alt=""  />
+<div className="dropDown">
+  <p>Sign Out Of Netflix</p>
+</div>
+
+    </div>
+    
+    
+    </div>
+    </div>
+  )
+}
+
+export default Navbar 
