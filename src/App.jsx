2bf4eecac3e9d476c6c29c44637b2ec37b56a7f3
SHILJIYA SHIRIN <sshiljiya@gmail.com>
2024-10-05 13:08:15 +0530

    mounted Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 
 import React from 'react'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Player from './pages/Player/Player'
 
 const App = () => {
   return (
     <div>
-      
+      <Home></Home>
+      <Login></Login>
     </div>
   )
 }
