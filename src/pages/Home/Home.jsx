d03090490273510da317a6ee859c2a1a65370715
SHILJIYA SHIRIN <sshiljiya@gmail.com>
2024-10-05 13:06:32 +0530

    Modified Home page and Mounted Navbar, TitleCards, Footer components into the home and beautifully created Homepage for netflix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import "./Home.css"
+import Navbar from '../../components/Navbar/Navbar'
+import Hero from '../../assets/hero_banner.jpg'
+import HeroTitle from '../../assets/hero_title.png'
+import PlayIcon from '../../assets/play_icon.png'
+import InfoIcon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards'
+import Footer from '../../components/Footer/Footer'
 const Home = () => {
   return (
     <div className='home'>
-      
+      <Navbar></Navbar>
+      <div className="hero">
+        <img src={Hero} alt="" className='banner-img' />
+      <div className="hero-caption">
+        <img src={HeroTitle} alt="" className='caption-img'/>
+        <p>Discovering hid ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+      <div className="hero-btns">
+        <button className='btn'><img src={PlayIcon} alt="" />Play</button>
+        <button className='btn dark-btn'><img src={InfoIcon} alt="" />More Info</button>
+      </div>
+      <TitleCards/>
+      </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"}/>
+         <TitleCards title={"Only On Netflix"}/> 
+         <TitleCards title={"Upcoming"}/>
+          <TitleCards title={"Top Picks For you"}/>
+      </div>
+      <Footer/>
     </div>
   )
 }
