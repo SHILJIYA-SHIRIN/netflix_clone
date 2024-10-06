5c5359aa2dfe61c92ee7bff9a9bd786aa7c74248
SHILJIYA SHIRIN <sshiljiya@gmail.com>
2024-10-06 22:37:01 +0530

    created firebase file and done with the autghntication for isgn in signup and enable sign out option also

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut} from "firebase/auth"
+import {addDoc, collection, getFirestore} from "firebase/firestore"
+import { toast } from "react-toastify";
 const firebaseConfig = {
   apiKey: "AIzaSyB8yvkFuAYplAcfPu0cUh3TADB_bB6GmfU",
   authDomain: "netflix-clone-d9e48.firebaseapp.com",
@@ -13,3 +15,33 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signUp = async(name,email,password)=>{
+try {
+  const res=  await createUserWithEmailAndPassword(auth, email, password);
+  const user =res.user;
+   await addDoc(collection(db,"user"),{
+    uid:user.uid,
+    name,
+    authProvider: "local",
+    email,
+   });
+} catch (error) {
+    console.log(error);
+  toast.error(error.code.split('/')[1].split('-').join(" "));
+}
+}
+const login = async(email, password)=>
+{
+    try {
+        await signInWithEmailAndPassword(auth,email,password)
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" ") );
+    }
+}
+const logOut=()=>{
+signOut(auth);
+}
+export {auth,db,login,logOut,signUp};
