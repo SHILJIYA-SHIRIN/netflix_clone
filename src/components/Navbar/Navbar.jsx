68e405130193b11dd1ea9b1e7950688080f6fb83
SHILJIYA SHIRIN <sshiljiya@gmail.com>
2024-10-06 22:39:07 +0530

    created firebase auth

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import searchIcon from '../../assets/search_icon.svg'
 import bellIcon from '../../assets/bell_icon.svg'
 import dropDownIcon from '../../assets/caret_icon.svg'
 import profileImage from '../../assets/profile_img.png'
+import { logOut } from '../../firebase'
 
 
 const Navbar = () => {
@@ -29,7 +30,9 @@ const Navbar = () => {
     <img src={profileImage} alt="" className='profile' />
     <img src={dropDownIcon} alt=""  />
 <div className="dropDown">
-  <p>Sign Out Of Netflix</p>
+  <p onClick={()=>{
+    logOut();
+  }}>Sign Out Of Netflix</p>
 </div>
 
     </div>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,24 @@
 import React, { useState } from 'react'
 import "./Login.css"
 import logo from '../../assets/logo.png'
+import { login, signUp } from '../../firebase'
 const Login = () => {
 
 const [signState, setSignState]= useState("Sign In")
+const [name ,setName]=useState("");
+const [email ,setEmail]=useState("");
+const [password ,setPassword]=useState("");
+
+
+const userAuth = async(event)=>{
+  event.preventDefault();
+  if(signState==="Sign In"){
+    await login(email,password);
+  }else{
+    await signUp(name,email,password);
+  }
+}
+
 
   return (
     <div className='login'>
@@ -11,11 +26,11 @@ const [signState, setSignState]= useState("Sign In")
     <div className="login-form">
       <h1>{signState}</h1>
       <form >
-        {signState==="Sign Up"?<input type="text"  placeholder='Your Name'/>:<></>}
+        {signState==="Sign Up"?<input value={name} onChange={(e)=>{setName(e.target.value)}} type="text"  placeholder='Your Name'/>:<></>}
         
-        <input type="email" placeholder='Your Email' />
-        <input type="password" placeholder='password' />
-        <button>{signState}</button>
+        <input value={email} onChange={(e)=>{setEmail(e.target.value)}}  type="email" placeholder='Your Email' />
+        <input value={password} onChange={(e)=>{setPassword(e.target.value)}}  type="password" placeholder='password' />
+        <button onClick={userAuth} type='submit'>{signState}</button>
         <div className="form-help">
           <div className="remember">
             <input type="checkbox" />
